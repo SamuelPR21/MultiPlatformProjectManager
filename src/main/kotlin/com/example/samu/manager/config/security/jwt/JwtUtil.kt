1e60e7c404790e673f3baa18cddcbcf2b1165677
Samuel A. Perdomo Fajardo <u20222208795@usco.edu.co>
2024-12-08 13:41:58 -0500

    Validacion de token y obtener subject del JWT

--- src/main/kotlin/com/example/samu/manager/config/security/jwt/JwtUtil.kt
+++ src/main/kotlin/com/example/samu/manager/config/security/jwt/JwtUtil.kt
@@ -2,6 +2,7 @@ package com.example.samu.manager.config.security.jwt
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
 import org.springframework.stereotype.Component
 import java.util.Date
 import java.util.concurrent.TimeUnit
@@ -26,5 +27,24 @@ class JwtUtil {
             .withIssuedAt(Date()) // Fecha de emisión
             .withExpiresAt(Date(System.currentTimeMillis() + expirationTime)) // Fecha de expiración
             .sign(algorithm) // Firma del token
+    }
+
+
+    fun validate(jwt: String): Boolean {
+        try {
+            JWT.require(Algorithm.HMAC256(secretKey))
+                .build()
+                .verify(jwt)
+            return true;
+        } catch (exception: JWTVerificationException) {
+            return false;
+        }
+    }
+
+    fun getUsername(jwt: String): String {
+        return  JWT.require(Algorithm.HMAC256(secretKey))
+                .build()
+                .verify(jwt)
+                .subject
     }
 }
