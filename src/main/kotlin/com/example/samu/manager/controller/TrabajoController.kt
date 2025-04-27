4e941b64364d0b7522d50a3562f69963b5bce148
Samuel A. Perdomo Fajardo <u20222208795@usco.edu.co>
2025-04-26 21:04:39 -0500

    Cambios en api trabajo y agrego ISSUE

--- src/main/kotlin/com/example/samu/manager/controller/TrabajoController.kt
+++ src/main/kotlin/com/example/samu/manager/controller/TrabajoController.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
@@ -34,6 +35,7 @@ class TrabajoController(
     fun getAllTrabajo(@AuthenticationPrincipal usuario: UserDetails?): ResponseEntity<List<TrabajoDTO>> {
         if (usuario == null) {
             println("El usuario no está autenticado o no se ha inyectado correctamente.")
+            println(SecurityContextHolder.getContext().getAuthentication().getPrincipal())
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
         }
 
